63d2eeb14410918cb5891514befd0d61efbb6b9d
Refaster24 <refaster24@gmail.com>
2023-07-16 13:24:26 +0300

    edit

--- src/app/job-detail/[id]/page.tsx
+++ src/app/job-detail/[id]/page.tsx
@@ -7,8 +7,9 @@ import JobDescription from '@/components/job-detail/jobDescription'
 import Footer from '@/components/job-detail/footer'
 
 export default function Home(){
-    const id = useParams().id - 1
-    const job = useAppSelector((state)=>state.sliced.global[id] )
+    const {id} = useParams()
+    const index = parseInt(id) - 1
+    const job = useAppSelector((state)=>state.sliced.global[index] )
 
     return (
         <>
